3fd036f612136a0fda1f88ba03c799ec5e75a48b
Joao Paulo <joaopaulodantasbernardo@gmail.com>
2021-09-03 00:47:35 -0300

    Resolução

---
+++ src/entities/Contract.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Contract {
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+	
+	List<Installment> installments = new ArrayList<>();
+	
+	public Contract(int number, Date date, Double totalValue) {
+		
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double value) {
+		this.totalValue = value;
+	}
+	
+	public List<Installment> getInstallments(){
+		return installments;
+	}
+	
+	public void addInstallment(Installment installment) {
+		installments.add(installment);
+	}
+	
+	public void removeInstallment(Installment installment) {
+		installments.remove(installment);
+	}
+	
+}
